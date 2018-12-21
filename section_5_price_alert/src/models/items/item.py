872afb17a1ab06686876d83b080ce84029274dc5
Sagarnil Das <sagarnildass@gmail.com>
2018-12-22 02:40:16 +0530

    Section_5 created file structures

---
+++ section_5_price_alert/src/models/items/item.py
@@ -0,0 +1,11 @@
+
+
+class Item(object):
+
+    def __init__(self, name, price, url):
+        self.name = name
+        self.price = price
+        self.url = url
+
+    def __repr__(self):
+        return "<Item {} with url {}>".format(self.name, self.url)
